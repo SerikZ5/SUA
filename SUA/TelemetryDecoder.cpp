98f56a592d851d7b3517bb55b7778f0d85a1c56b
Serik <Serik_z5@mail.ru>
2017-07-13 16:03:11 +0300

    dsfsd

---
+++ SUA/TelemetryDecoder.cpp
@@ -0,0 +1,10 @@
+#include "TelemetryDecoder.h"
+
+TelemetryDecored::TelemetryDecored()
+{
+}
+
+TelemetryDecored::~TelemetryDecored()
+{
+
+}
--- SUA/TelemetryDecoder.h
+++ SUA/TelemetryDecoder.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef TELEMETRY_DECODER
+#define TELEMETRY_DECODER
+
+#include <QByteArray>
+
+class TelemetryDecored
+{
+public:
+  TelemetryDecored();
+  virtual ~TelemetryDecored();
+
+protected:
+  QByteArray inputBytes;
+};
+
+#endif
